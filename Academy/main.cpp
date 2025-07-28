47f140bda842aadeb1635e1a013cb3c05a5f4bee
supamodd <ghosts.zz@yandex.ru>
2025-07-28 11:14:57 +0300

    Academy and Files HW DONE

--- Academy/main.cpp
+++ Academy/main.cpp
@@ -1,5 +1,8 @@
-#include<iostream>
-#include<string>
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <vector>
+
 //using namespace std;
 using std::cin;
 using std::cout;
@@ -50,7 +53,7 @@ public:
 		this->age = age;
 	}
 
-	//				Constructors:
+	//				Конструкторы:
 	Human(HUMAN_TAKE_PARAMETERS)
 	{
 		set_last_name(last_name);
@@ -65,25 +68,38 @@ public:
 		cout << "HDestructor:\t" << this << endl;
 	}
 
-	//				  Methods:
+	//				  Методы:
 	virtual std::ostream& info(std::ostream& os)const
 	{
 		//return os << last_name << " " << first_name << " " << age;
-		os.width(LAST_NAME_WIDTH);	//метод width(N) задает ширину вывода в 'N' знакопозиций,
-		//если выводимая строка меньше 'N', то недостающие символы заполняются пробелами,
-		//если выводимая строка больше 'N', она выводится полностью, и общее выравнивание нарушается
-		os << std::left;//первый вызов width() задает выравнивание по правому краю заданного поля, 
-		//и его нужно явно выровнять по левому краю.
+		os.width(LAST_NAME_WIDTH);	//ширина поля для фамилии
+		os << std::left; //выравнивание влево
 		os << last_name;
-		os.width(FIRST_NAME_WIDTH);
+		os.width(FIRST_NAME_WIDTH); //ширина поля для имени
 		os << first_name;
-		os.width(AGE_WIDTH);
+		os.width(AGE_WIDTH); //ширина поля для возраста
 		os << age;
 		return os;
 	}
+
+	virtual void save(std::ofstream& ofs) const 
+	{
+		ofs << "Human\n"; //Сохраняем тип класса
+		ofs << last_name << "\n"; //Сохраняем фамилию
+		ofs << first_name << "\n"; //Сохраняем имя
+		ofs << age << "\n"; //Сохраняем возраст
+	}
+
+	virtual void load(std::ifstream& ifs) 
+	{
+		std::getline(ifs, last_name); //Загружаем фамилию
+		std::getline(ifs, first_name); //Загружаем имя
+		ifs >> age; //Загружаем возраст
+		ifs.ignore(); // Пропускаем символ новой строки после возраста
+	}
 };
 //static member definition:
-int Human::count = 0;	//Статические переменные можно инициализировать только за пределами класса.
+int Human::count = 0;	//Определение статической переменной - обязательно вне класса.
 std::ostream& operator<<(std::ostream& os, const Human& obj)
 {
 	return obj.info(os);
@@ -100,8 +116,8 @@ class Student :public Human
 	static const int RATING_WIDTH = 5;
 	std::string speciality;
 	std::string group;
-	double rating;		//успеваемость
-	double attendance;	//посещаемость
+	double rating;		//Оценка
+	double attendance;	//Посещаемость
 public:
 	const std::string& get_speciality()const
 	{
@@ -136,7 +152,7 @@ public:
 		this->attendance = attendance;
 	}
 
-	//				Constructors:
+	//				Конструкторы:
 	Student
 	(
 		HUMAN_TAKE_PARAMETERS,
@@ -154,10 +170,10 @@ public:
 		cout << "SDestructor:\t" << this << endl;
 	}
 
-	//					Methods:
+	//					Методы:
 	std::ostream& info(std::ostream& os)const override
 	{
-		Human::info(os);
+		Human::info(os); //Вызываем метод info базового класса
 		os.width(SPECIALITY_WIDTH);
 		os << speciality;
 		os.width(GROUP_WIDTH);
@@ -170,6 +186,26 @@ public:
 
 		//Human::info(os) << " ";
 		//return os << speciality << " " << group << " " << rating << " " << attendance;
+	}
+	void save(std::ofstream& ofs) const override
+	{
+		ofs << "Student\n"; //Сохраняем тип класса
+		Human::save(ofs); // Вызываем метод save базового класса Human для сохранения его данных.
+		ofs << speciality << "\n"; //Сохраняем специальность
+		ofs << group << "\n"; //Сохраняем группу
+		ofs << rating << "\n"; //Сохраняем оценку
+		ofs << attendance << "\n"; //Сохраняем посещаемость
+	}
+
+	void load(std::ifstream& ifs) override 
+	{
+		Human::load(ifs); // Загружаем данные базового класса Human
+		std::getline(ifs, speciality); //Загружаем специальность
+		std::getline(ifs, group); //Загружаем группу
+		ifs >> rating; //Загружаем оценку
+		ifs.ignore(); // Пропускаем символ новой строки после оценки
+		ifs >> attendance; //Загружаем посещаемость
+		ifs.ignore(); // Пропускаем символ новой строки после посещаемости
 	}
 };
 
@@ -200,7 +236,7 @@ public:
 		this->experience = experience;
 	}
 
-	//				Constructors:
+	//				Конструкторы:
 	Teacher
 	(
 		HUMAN_TAKE_PARAMETERS,
@@ -216,10 +252,10 @@ public:
 		cout << "TDestructor:\t" << this << endl;
 	}
 
-	//				Methods:
+	//				Методы:
 	std::ostream& info(std::ostream& os)const override
 	{
-		Human::info(os);
+		Human::info(os); //Вызываем метод info базового класса
 		os.width(SPECIALITY_WIDTH);
 		os << speciality;
 		os.width(EXPERIENCE_WIDTH);
@@ -227,6 +263,18 @@ public:
 		return os;
 		//Human::info(os) << " ";
 		//return os << speciality << " " << experience;
+	}
+	void save(std::ofstream& ofs) const override {
+		ofs << "Teacher\n"; //Сохраняем тип класса
+		Human::save(ofs); // Сохраняем данные базового класса Human
+		ofs << speciality << "\n"; //Сохраняем специальность
+		ofs << experience << "\n"; //Сохраняем опыт
+	}
+	void load(std::ifstream& ifs) override {
+		Human::load(ifs); // Загружаем данные базового класса Human
+		std::getline(ifs, speciality); //Загружаем специальность
+		ifs >> experience; //Загружаем опыт
+		ifs.ignore(); // Пропускаем символ новой строки после опыта
 	}
 };
 
@@ -249,42 +297,95 @@ public:
 		Student::info(os) << " ";
 		return os << subject;
 	}
+	void save(std::ofstream& ofs) const override
+	{
+		ofs << "Graduate\n"; //Сохраняем тип класса
+		Student::save(ofs); // Сохраняем данные базового класса Student
+		ofs << subject << "\n"; //Сохраняем тему дипломной работы
+	}
+
+	void load(std::ifstream& ifs) override {
+		Student::load(ifs); // Загружаем данные базового класса Student
+		std::getline(ifs, subject); //Загружаем тему дипломной работы
+	}
 };
 
-//#define INHERITANCE
-#define POLYMORPHISM	//poly - много, morphis - форма.
+// Функция для сохранения группы в файл
+void saveGroupToFile(const std::vector<Human*>& group, const std::string& filename) 
+{
+	std::ofstream ofs(filename); // Открываем файл для записи
+	if (ofs.is_open()) {
+		ofs << group.size() << "\n";  // Сохраняем количество элементов в группе.
+		for (const Human* person : group) {
+			person->save(ofs); // Сохраняем каждый объект в файле, используя полиморфизм.
+		}
+		ofs.close(); // Закрываем файл
+		std::cout << "Группа сохранена в файл: " << filename << std::endl;
+	}
+	else {
+		std::cerr << "Не удалось открыть файл для записи: " << filename << std::endl;
+	}
+}
+
+// Функция для загрузки группы из файла
+std::vector<Human*> loadGroupFromFile(const std::string& filename) 
+{
+	std::vector<Human*> group; // Вектор для хранения загруженной группы
+	std::ifstream ifs(filename); // Открываем файл для чтения
+	if (ifs.is_open()) {
+		int groupSize;
+		ifs >> groupSize; // Читаем количество элементов в группе
+		ifs.ignore(); // Пропускаем символ новой строки после количества
+
+		for (int i = 0; i < groupSize; ++i) {
+			std::string classType;
+			std::getline(ifs, classType); // Читаем тип класса из файла
+
+			Human* person = nullptr;
+			if (classType == "Human") {
+				person = new Human("", "", 0); // Создаем объект Human
+			}
+			else if (classType == "Student") {
+				person = new Student("", "", 0, "", "", 0, 0); // Создаем объект Student
+			}
+			else if (classType == "Teacher") {
+				person = new Teacher("", "", 0, "", 0); // Создаем объект Teacher
+			}
+			else if (classType == "Graduate") {
+				person = new Graduate("", "", 0, "", "", 0, 0, ""); // Создаем объект Graduate
+			}
+			else {
+				std::cerr << "Неизвестный тип класса в файле: " << classType << std::endl;
+				ifs.close();
+				// Освобождаем выделенную память
+				for (Human* h : group) {
+					delete h;
+				}
+				return {}; // Возвращаем пустой вектор
+			}
+
+			if (person) {
+				person->load(ifs); // Загружаем данные объекта из файла, используя полиморфизм.
+				group.push_back(person); // Добавляем объект в группу
+			}
+		}
+
+		ifs.close(); // Закрываем файл
+		std::cout << "Группа загружена из файла: " << filename << std::endl;
+	}
+	else {
+		std::cerr << "Не удалось открыть файл для чтения: " << filename << std::endl;
+	}
+	return group; // Возвращаем загруженную группу
+}
 
 void main()
 {
 	setlocale(LC_ALL, "");
 
-#ifdef INHERITANCE
-	Human human("Montana", "Antonio", 25);
-	human.info();
-
-	Student student("Pinkman", "Jessie", 22, "Chemistry", "WW_220", 95, 99);
-	student.info();
-
-	Teacher teacher("White", "Walter", 50, "Chemistry", 25);
-	teacher.info();
-
-	Graduate graduate("Schreder", "Hank", 40, "Criminalistic", "WW_220", 40, 60, "How to catch Heisenberg");
-	graduate.info();
-#endif // INHERITANCE
-	 
-	/*
-	---------------------------------------------------
-	1. Ad-hoc polymorphism;
-	2. Inclusion polymorphism (полиморфизм подтипов):
-		Указатели на базовый класс (Base class pointers): Generalisation, Upcast;
-		Виртуальные методы: vfptr (Virtual Function pointers) - таблица указателей на виртуальные функции;
-	   Runtime polymorphism;
-	---------------------------------------------------
-	*/
-
-	Human* group[] =
+	// Создаем группу людей
+	std::vector<Human*> group = 
 	{
-		//Приведение дочернего объекта к базовому типу называют Upcast
 		new Human("Montana", "Antonio", 25),
 		new Student("Pinkman", "Jessie", 22, "Chemistry", "WW_220", 95, 99),
 		new Teacher("White", "Walter", 50, "Chemistry", 25),
@@ -294,18 +395,41 @@ void main()
 		new Graduate("Targarian", "Daineris", 22, "Flight", "GoT", 91, 92, "How to make smoke"),
 		new Teacher("Schwartzneger", "Arnold", 85, "Heavy Metal", 60)
 	};
-	for (int i = 0; i < sizeof(group) / sizeof(group[0]); i++)
+
+	// Выводим группу на экран
+	cout << "Исходная группа:\n";
+	for (const Human* person : group) 
 	{
-		//group[i]->info();
-		cout << *group[i] << endl;
+		cout << *person << endl;
 		cout << delimiter << endl;
 	}
-	cout << "Количество людей: " << group[0]->get_count() << endl;
-	for (int i = 0; i < sizeof(group) / sizeof(group[0]); i++)
-	{
-		delete group[i];
-	}
-	cout << "Количество людей: " << group[0]->get_count() << endl;
-	//cout << "Количество людей: " << Human::get_count() << endl;
 
+	// Сохраняем группу в файл
+	saveGroupToFile(group, "group.txt");
+
+	// Освобождаем память, выделенную для исходной группы
+	for (Human* person : group) 
+	{
+		delete person;
+	}
+	group.clear();
+
+	// Загружаем группу из файла
+	std::vector<Human*> loadedGroup = loadGroupFromFile("group.txt");
+
+	// Выводим загруженную группу на экран
+	cout << "\nЗагруженная группа:\n";
+	if (!loadedGroup.empty()) {
+		for (const Human* person : loadedGroup) 
+		{
+			cout << *person << endl;
+			cout << delimiter << endl;
+		}
+	}
+
+	// Освобождаем память, выделенную для загруженной группы
+	for (Human* person : loadedGroup) 
+	{
+		delete person;
+	}
 }
